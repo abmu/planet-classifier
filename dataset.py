3331750d6782b555f021605583b212e65c1a9a4d
Abdul Muhaymin Abdul Hafiz <mr-abdul@live.co.uk>
2025-02-26 14:14:20 +0000

    Added get data loaders function

--- dataset.py
+++ dataset.py
@@ -1,14 +1,23 @@
 from typing import Callable, Optional
 from torch.utils.data import Dataset, DataLoader
+import torchvision.transforms as transforms
 from torchvision.datasets import ImageFolder
 from PIL.Image import Image
 
 
 class PlanetsDataset(Dataset):
     def __init__(self, data_dir: str, transform: Optional[Callable]=None) -> None:
+        '''
+        Args:
+            data_dir: path to the dataset train, valid, or test folder
+            transform: transform to be applied on data
+        '''
         self.data = ImageFolder(data_dir, transform=transform)
 
     def __len__(self) -> int:
+        '''
+        Returns the size of the dataset
+        '''
         return len(self.data)
 
     def __getitem__(self, idx: int) -> tuple[Image, int]:
@@ -19,7 +28,34 @@ class PlanetsDataset(Dataset):
     
     @property
     def classes(self) -> list[str]:
+        '''
+        Returns the different classes in the dataset
+        '''
         return self.data.classes
 
-dataset = PlanetsDataset(data_dir='./dataset/train')
-print(len(dataset), dataset[0], dataset[1048])
+
+def get_dataloaders(train_dir: str, valid_dir: str, test_dir: str, batch_size: int=32):
+    '''
+    Args:
+        train_dir: path to the train folder
+        valid_dir: path to the valid folder
+        test_dir: path to the test folder
+        batch_size: number of samples per batch, default 32
+
+    Returns a tuple containing the data loaders for the train, valid, and test datasets
+    '''
+
+    transform = transforms.Compose([
+        transforms.Resize((128,128)),
+        transforms.ToTensor(),
+    ])
+
+    train_dataset = PlanetsDataset(data_dir=train_dir, transform=transform)
+    valid_dataset = PlanetsDataset(data_dir=valid_dir, transform=transform)
+    test_dataset = PlanetsDataset(data_dir=test_dir, transform=transform)
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size)
+    valid_loader = DataLoader(valid_dataset, batch_size=batch_size)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size)
+
+    return train_loader, valid_loader, test_loader
