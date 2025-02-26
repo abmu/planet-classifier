af0038626ba5bd44220560181026659e2940b190
Abdul Muhaymin Abdul Hafiz <mr-abdul@live.co.uk>
2025-02-26 10:01:37 +0000

    Changed type hint

--- dataset.py
+++ dataset.py
@@ -1,6 +1,7 @@
-from typing import Any, Callable, Optional
+from typing import Callable, Optional
 from torch.utils.data import Dataset, DataLoader
 from torchvision.datasets import ImageFolder
+from PIL.Image import Image
 
 
 class PlanetsDataset(Dataset):
@@ -10,9 +11,15 @@ class PlanetsDataset(Dataset):
     def __len__(self) -> int:
         return len(self.data)
 
-    def __getitem__(self, idx: int) -> tuple[Any, Any]:
+    def __getitem__(self, idx: int) -> tuple[Image, int]:
+        '''
+        Returns a tuple containing the image and its class label (integer in the range 0-9)
+        '''
         return self.data[idx]
     
     @property
     def classes(self) -> list[str]:
         return self.data.classes
+
+dataset = PlanetsDataset(data_dir='./dataset/train')
+print(len(dataset), dataset[0], dataset[1048])
